393650725cdea9f94a53a84cb4f48320dd9f662a
kelly r maass <kmaass77627@gmail.com>
2019-10-20 00:37:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length == 1 
-    puts array[0]
+     array[0]
   elsif array.length == 2 
-    puts array[0]<< " and " << array[1]
+     array[0]<< " and " << array[1]
   else array.length >= 3 
-    puts array[0..(array.length-2)].join(", ")<< ", and " << array[array.length-1]
+     array[0..(array.length-2)].join(", ")<< ", and " << array[array.length-1]
   end 
 end 
 
