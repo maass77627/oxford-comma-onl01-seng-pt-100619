404728241fa0fffbce531434fbbf10cb26ee3e69
kelly r maass <kmaass77627@gmail.com>
2019-10-20 00:31:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elseif array.length == 2 
     puts array[0]<< and << array[1]
   else array.length >= 3 
-    puts array[0..(array.length-2)].join(", ")<< ,and << array.length-1
+    puts array[0..(array.length-2)].join(", ")<< ",and" << array.length-1
   end 
 end 
 
